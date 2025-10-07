d291f4e7de3eb4a50168827c60d8ee24128a014f
c0a24127ee <c0a24127ee@edu.teu.ac.jp>
2025-10-07 14:05:46 +0900

    Beamクラス実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -23,7 +23,6 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
-
 class Bird:
     """
     ゲームキャラクター（こうかとん）に関するクラス
@@ -83,31 +82,29 @@ class Bird:
             self.img = __class__.imgs[tuple(sum_mv)]
         screen.blit(self.img, self.rct)
 
+# class Beam:
+#     """
+#     こうかとんが放つビームに関するクラス
+#     """
+#     def __init__(self, bird:"Bird"):
+#         """
+#         ビーム画像Surfaceを生成する
+#         引数 bird：ビームを放つこうかとん（Birdインスタンス）
+#         """
+#         self.img = pg.image.load(f"fig/beam.png")  # 画像ロード
+#         self.rct = self.img.get_rect()
+#         self.rct.centery = bird.rct.centery
+#         self.rct.left = bird.rct.right
+#         self.vx, self.vy = +5, 0
 
-# ビームクラス:
-    # """
-    # こうかとんが放つビームに関するクラス
-    # """
-    # def イニシャライザ(self, bird:"Bird"):
-    #     """
-    #     ビーム画像Surfaceを生成する
-    #     引数 bird：ビームを放つこうかとん（Birdインスタンス）
-    #     """
-    #     self.img = pg.画像のロード(f"fig/beam.png")
-    #     self.rct = self.img.Rectの取得()
-    #     self.ビームの中心縦座標 = こうかとんの中心縦座標
-    #     self.ビームの左座標 = こうかとんの右座標
-    #     self.vx, self.vy = +5, 0
-
-    # def update(self, screen: pg.Surface):
-    #     """
-    #     ビームを速度ベクトルself.vx, self.vyに基づき移動させる
-    #     引数 screen：画面Surface
-    #     """
-    #     if check_bound(self.rct) == (True, True):
-    #         self.rct.move_ip(self.vx, self.vy)
-    #         screen.blit(self.img, self.rct)    
-
+#     def update(self, screen: pg.Surface):
+#         """
+#         ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+#         引数 screen：画面Surface
+#         """
+#         if check_bound(self.rct) == (True, True):
+#             self.rct.move_ip(self.vx, self.vy)
+#             screen.blit(self.img, self.rct)    
 
 class Bomb:
     """
@@ -139,7 +136,6 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
-
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -153,9 +149,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            # if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-            #     # スペースキー押下でBeamクラスのインスタンス生成
-            #     beam = Beam(bird)            
+        #     if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+        #         # スペースキー押下でBeamクラスのインスタンス生成
+        #         beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
         if bird.rct.colliderect(bomb.rct):
@@ -172,7 +168,6 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
